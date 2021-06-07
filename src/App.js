137f18000defaea34a216212d4822b1c8727ae8a
Tom <tblackmore0@gmail.com>
2021-06-07 11:53:45 +0100

    Add basic form to first component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import Header from './Components/Header'
+import Personal from './Components/Personal'
+import Education from './Components/Education'
+import Work from './Components/Work'
+import Options from './Components/Options'
+
 function App() {
   return (
-    <div>Hello!</div>
+    <div>
+      <Header />
+      <Personal />
+      <Education />
+      <Work />
+      <Options />
+    </div>
   );
 }
 
